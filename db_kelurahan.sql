5d2f829f1451356fb051de552e2a9760225b11a9
gil037 <dorayaki.lawersclub@gmail.com>
2022-06-10 16:30:52 +0800

    PERBAHARUI DATABSE

--- db_kelurahan.sql
+++ db_kelurahan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2022 at 10:07 AM
+-- Generation Time: Jun 10, 2022 at 10:28 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.5
 
@@ -36,6 +36,22 @@ CREATE TABLE `tb_datapegawai` (
   `Nip` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tb_datapegawai`
+--
+
+INSERT INTO `tb_datapegawai` (`Id_Pegawai`, `Id`, `Jabatan`, `Nama`, `Alamat`, `Jenis_Kelamin`, `Tempat_Lahir`, `Tanggal_Lahir`, `Agama`, `Status`, `Kewarganegaraan`, `Nip`) VALUES
+(1, '#KCD349843\n', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1),
+(2, '#ULV171521\n', '2', '2', '2', '2', '2', '2', '2', '2', '2', 2),
+(3, '#EZA268286\n', '3', '3', '3', '3', '3', '3', '3', '3', '3', 3),
+(4, '#XFD707276\n', '4', '4', '4', '4', '4', '4', '4', '4', '4', 4),
+(5, '#FTD039091\n', '5', '5', '5', '5', '5', '5', '5', '5', '5', 5),
+(6, '#ERD350823\n', '6', '6', '6', '6', '6', '6', '6', '6', '6', 6),
+(7, '#GTL308040\n', '7', '7', '7', '7', '7', '7', '7', '7', '7', 7),
+(8, '#YON745368\n', '8', '8', '8', '8', '8', '8', '8', '8', '8', 8),
+(9, '#GHA128523\n', '9', '9', '9', '9', '9', '9', '9', '9', '9', 9),
+(10, '#IOA629201\n', '10', '10', '10', '10', '10', '10', '10', '10', '10', 10);
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +72,27 @@ CREATE TABLE `tb_datapenduduk` (
   `Kewarganegaraan` varchar(40) NOT NULL,
   `Nik` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_datapenduduk`
+--
+
+INSERT INTO `tb_datapenduduk` (`Id_Penduduk`, `Id`, `Nama`, `Alamat`, `Jenis_Kelamin`, `Tempat_Lahir`, `Tanggal_Lahir`, `Agama`, `Pekerjaan`, `Status`, `Kewarganegaraan`, `Nik`) VALUES
+(1, '#ZNW479669\n', '1', '1', '1', '1', '1', '1', '1', '1', '1', 1),
+(2, '#PAJ779280\n', '2', '2', '2', '2', '2', '2', '2', '2', '2', 2),
+(3, '#HHO357743\n', '3', '3', '3', '3', '3', '3', '3', '3', '3', 3),
+(4, '#WAD495391\n', '4', '4', '4', '4', '4', '4', '4', '4', '4', 4),
+(5, '#EJM877648\n', '5', '5', '5', '5', '5', '5', '5', '5', '5', 5),
+(6, '#DYY915914\n', '6', '6', '6', '6', '6', '6', '6', '6', '6', 6),
+(7, '#AKR256182\n', '7', '7', '7', '7', '7', '7', '7', '7', '7', 7),
+(8, '#SKZ782684\n', '7', '7', '7', '7', '8', '8', '8', '8', '8', 8),
+(9, '#VOC910037\n', '9', '9', '9', '9', '9', '9', '9', '9', '9', 9),
+(10, '#PCD165902\n', '10', '10', '10', '10', '10', '10', '10', '10', '10', 10),
+(11, '#RBN305702\n', '1', '1', '1', '11', '11', '11', '11', '11', '11', 11),
+(12, '#FRH914936\n', '12', '12', '12', '12', '12', '12', '12', '12', '12', 12),
+(13, '#QHI142234\n', '13', '13', '13', '13', '13', '13', '13', '13', '13', 13),
+(14, '#LDT105644\n', '14', '14', '14', '14', '14', '14', '14', '14', '14', 14),
+(15, '#HEH929663\n', '15', '15', '15', '15', '15', '15', '15', '15', '15', 15);
 
 --
 -- Indexes for dumped tables
@@ -81,11 +118,11 @@ ALTER TABLE `tb_datapenduduk`
 -- AUTO_INCREMENT for table `tb_datapegawai`
 --
 ALTER TABLE `tb_datapegawai`
-  MODIFY `Id_Pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id_Pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tb_datapenduduk`
 --
 ALTER TABLE `tb_datapenduduk`
-  MODIFY `Id_Penduduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2009106121;
+  MODIFY `Id_Penduduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
